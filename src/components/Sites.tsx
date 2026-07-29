113aab307a04b648d871c4d31c951caf069e5e4b
martingerdin <martingerdin@gmail.com>
2026-07-29 17:32:56 +0200

    Fix Map component width

--- src/components/Sites.tsx
+++ src/components/Sites.tsx
@@ -6,7 +6,7 @@ export default class Sites extends Component {
   template() {
     return (
       <section class="section" id="sites">
-        <div class="section__inner section__inner--wide">
+        <div class="section__inner">
           <h2 class="section__heading">{sites.title}</h2>
           <p class="section__subheading">{sites.subtitle}</p>
           <SitesMap />
