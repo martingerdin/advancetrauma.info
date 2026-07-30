78d7bd646aae51c05c8aa493c060b5cf7f9361a8
martingerdin <martingerdin@gmail.com>
2026-07-30 22:26:29 +0200

    Close #20 Add pictures to the resource cards

--- src/components/Resources.tsx
+++ src/components/Resources.tsx
@@ -16,8 +16,20 @@ export default class Resources extends Component {
               href={resources.items[0].href}
               download
             >
-              <h3 class="resource-card__title">{resources.items[0].label}</h3>
-              <p class="resource-card__description">{resources.items[0].description}</p>
+              <div class="resource-card__media">
+                <img
+                  src={resources.items[0].image}
+                  alt={resources.items[0].imageAlt}
+                  width="1200"
+                  height="900"
+                  loading="lazy"
+                  decoding="async"
+                />
+              </div>
+              <div class="resource-card__body">
+                <h3 class="resource-card__title">{resources.items[0].label}</h3>
+                <p class="resource-card__description">{resources.items[0].description}</p>
+              </div>
             </a>
             <a
               class="resource-card"
@@ -25,8 +37,20 @@ export default class Resources extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h3 class="resource-card__title">{resources.items[1].label}</h3>
-              <p class="resource-card__description">{resources.items[1].description}</p>
+              <div class="resource-card__media">
+                <img
+                  src={resources.items[1].image}
+                  alt={resources.items[1].imageAlt}
+                  width="1200"
+                  height="900"
+                  loading="lazy"
+                  decoding="async"
+                />
+              </div>
+              <div class="resource-card__body">
+                <h3 class="resource-card__title">{resources.items[1].label}</h3>
+                <p class="resource-card__description">{resources.items[1].description}</p>
+              </div>
             </a>
             <a
               class="resource-card"
@@ -34,8 +58,20 @@ export default class Resources extends Component {
               target="_blank"
               rel="noopener noreferrer"
             >
-              <h3 class="resource-card__title">{resources.items[2].label}</h3>
-              <p class="resource-card__description">{resources.items[2].description}</p>
+              <div class="resource-card__media">
+                <img
+                  src={resources.items[2].image}
+                  alt={resources.items[2].imageAlt}
+                  width="1200"
+                  height="900"
+                  loading="lazy"
+                  decoding="async"
+                />
+              </div>
+              <div class="resource-card__body">
+                <h3 class="resource-card__title">{resources.items[2].label}</h3>
+                <p class="resource-card__description">{resources.items[2].description}</p>
+              </div>
             </a>
           </div>
         </div>
